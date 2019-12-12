73e23f1a1defbddc0ea2fbaff21e979385c1963d
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-12 01:07:44 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,10 @@
 # Write your code here.
 def line(customers)
-  pp customers
+  case customers.length
+ when 0
+   puts "The line is currently empty."
+ else
+  new_array= customers.each_with_index.map {|item, index|  "#{index+1}. #{item}"}
+  puts "The line is currently: #{new_array.join(" ")}"
+ end
 end
